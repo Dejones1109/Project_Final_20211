8d486883935556dad6f12f8a1721dc1014f196b5
Dejones1109 <tranvande19980911@gmail.com>
2021-12-08 23:39:37 +0700

    base RestAPI

---
+++ BE/papa/src/test/java/com/project/final/PapaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
