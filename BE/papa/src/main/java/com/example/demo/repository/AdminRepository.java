01a17b7d9d10775c14710fc40b51fb52ac650783
Dejones1109 <tranvande19980911@gmail.com>
2021-12-10 00:05:22 +0700

    base repository done and connect to hirbenate

--- BE/papa/src/main/java/com/example/demo/repository/AdminRepository.java
+++ BE/papa/src/main/java/com/example/demo/repository/AdminRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.repository;
 
-public class AdminRepository {
+import com.example.demo.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin,String> {
 }
